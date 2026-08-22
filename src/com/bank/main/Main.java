a62d94872413e8b8d879ecd72be37d63eca9a114
khisan <khisan8@gmail.com>
2026-08-22 11:39:48 +0700

    feat: add list choices menu scanner

--- src/com/bank/main/Main.java
+++ src/com/bank/main/Main.java
@@ -1,7 +1,31 @@
 package com.bank.main;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Bank Application!");
-    }
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Pilih Menu:");
+      System.out.println("1. Transfer");
+      System.out.println("2. Cek Saldo");
+      System.out.println("3. Deposit");
+      System.out.println("4. Transfer");
+      System.out.println("5. Keluar");
+
+      System.out.println("Masukkan pilihan Anda: ");
+      int choice = scanner.nextInt();
+      if (choice == 1) {
+            System.out.println("Transfer");
+        } else if (choice == 2) {
+            System.out.println("Cek Saldo");
+        } else if (choice == 3) {
+            System.out.println("Deposit");
+        } else if (choice == 4) {
+            System.out.println("Transfer");
+        } else if (choice == 5) {
+            System.out.println("Keluar");
+        } else {
+            System.out.println("Pilihan tidak valid.");
+        }
+      }
 }
