6bf63f1b8e73900ae36ee42009cd2f932d0dc497
khisan <khisan8@gmail.com>
2026-08-22 19:20:01 +0700

    feat: add CheckingAccount class with interest rate and applyInterest method

---
+++ src/com/bank/model/CheckingAccount.java
@@ -0,0 +1,19 @@
+package com.bank.model;
+public class CheckingAccount extends Account {
+    private double interestRate;
+
+    public CheckingAccount(String accountNumber, double balance, String owner, double interestRate) {
+        super(accountNumber, balance, owner);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+      return interestRate;
+    }
+    
+    public void applyInterest() {
+        double interest = getBalance() * interestRate;
+        deposit(interest);
+        System.out.println("Bunga sebesar " + interest + " telah diterapkan. Saldo saat ini: " + getBalance());
+    }
+}
