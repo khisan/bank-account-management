0cc01139d60f82886684fddb0e1b3ff6b13343e7
khisan <khisan8@gmail.com>
2026-08-23 16:38:46 +0700

    feat: implement Customer class with properties and constructor for customer details

---
+++ src/com/bank/model/Customer.java
@@ -0,0 +1,46 @@
+package com.bank.model;
+
+import java.util.List;
+
+public class Customer {
+
+    private String Id;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private List<Account> accounts;
+
+    public Customer(String id, String name, String email, String address, String phoneNumber, List<Account> accounts) {
+        this.Id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.accounts = accounts;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+}
